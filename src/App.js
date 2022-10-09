f73d0762dd3679567a02da3607ccfe21a360e355
szlegp <szlegp@gmail.com>
2022-10-10 00:39:55 +0200

    Screen extras panel.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-        <h1 className="app-title">Twój nowy <img className="app-title-icon" src="laptop.png"></img></h1>
-        <div className="progress-bar"><p className="progress-bar-text">1 / 2 / 3 / Do czego użyjesz laptopa</p></div> 
+      <h1 className="app-title">Twój nowy <img className="app-title-icon" src="laptop.png"></img></h1>
+      <div className="progress-bar">
+        <p className="progress-bar-text">
+          ...
+          <span className="progress-bar-divider"></span>
+          <span className="progress-bar-divider-space"></span>
+          4
+          <span className="progress-bar-divider"></span>
+          <span className="progress-bar-divider-space"></span>
+          5
+          <span className="progress-bar-divider"></span>
+          <span className="progress-bar-divider-space"></span>
+          Do czego użyjesz laptopa?</p>
+      </div>
+      <div className="content">
+        <div className="choice-button">
+          <img className="choice-image" src="icons8-touchscreen-64.png"></img>
+          <p className="choice-text">
+            Ekran dotykowy pozwala wybierać elementy na ekranie naciskając na nie palcem, niestety ekran łatwo jest ubrudzić.
+          </p>
+        </div>
+        <p class="skip-button">
+          Pomiń
+        </p>
+
+      </div>
     </div>
   );
 }

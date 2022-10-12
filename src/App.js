f76bc786acf8c1f7d0918700dd4ad446c141de91
szlegp <szlegp@gmail.com>
2022-10-12 23:29:11 +0200

    Comparison page.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="app">
       <h1 className="app-title">
-        Twój nowy 
+        Twój nowy
         <img className="app-title-icon" src="laptop.png"></img>
-        
-        <img src="icons8-star-32.png" class="favs-star"></img>
-        <span class="favs-count">3</span>
+
+        <img src="icons8-star-32.png" className="favs-star"></img>
+        <span className="favs-count">3</span>
       </h1>
-      
+
       <div className="progress-bar">
         <p className="progress-bar-text">
           ...
@@ -25,18 +25,36 @@ function App() {
           <span className="progress-bar-divider-space"></span>
           Do czego użyjesz laptopa?</p>
       </div>
-      <div className="content">
+
+      <div style={{ "display": "none" }} className="content">
         <div className="choice-button">
           <img className="choice-image" src="icons8-touchscreen-64.png"></img>
           <p className="choice-text">
             Ekran dotykowy pozwala wybierać elementy na ekranie naciskając na nie palcem, niestety ekran łatwo jest ubrudzić.
           </p>
         </div>
-        <p class="skip-button">
+        <p className="skip-button">
           Pomiń
         </p>
-
       </div>
+
+
+      <table className="comparison-table">
+        <tbody>
+          <tr>
+            <th>
+              <img src="laptop.png" />
+              Lenovo L</th>
+            <th>
+              <img src="laptop.png" />HP Ultra Light</th>
+          </tr>
+          <tr>
+            <td>CPU<br />Pentium 2 Core</td>
+            <td>CPU<br />Pentium 4 Core</td>
+          </tr>
+        </tbody>
+      </table>
+
     </div>
   );
 }

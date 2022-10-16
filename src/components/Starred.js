22c8c32b1967395263c4e2892728d1f06500610c
szlegp <szlegp@gmail.com>
2022-10-16 23:31:36 +0200

    Star links to selection screen.

--- src/components/Starred.js
+++ src/components/Starred.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Starred() {
-    return (<>
+    return (<Link to="/select">
         <img src="icons8-star-32.png" className="favs-star" alt="star"></img>
         <span className="favs-count">3</span>
-    </>)
+    </Link>)
 }
 
 export default Starred;

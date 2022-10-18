3ea387c59d4ab96f2954ed739fac31330eee4e9d
szlegp <szlegp@gmail.com>
2022-10-18 17:27:15 +0200

    api prototype

---
+++ src/api.js
@@ -0,0 +1,22 @@
+export class Laptop {
+    constructor(id, name, image) {
+      this.id = id;
+      this.name = name;
+      this.image = image;
+    }
+  }
+  
+// returns ids of laptops that follow the filters 
+export function getLaptopIds(filters) {
+
+}
+
+// get laptop class of laptop with given id 
+export function getLaptop(laptopId) {
+
+}
+
+// get a object with laptop details, example {cpu: "pentium 2 core", gpu: "integrated"}
+export function getLaptopDetails(laptopId) {
+
+}

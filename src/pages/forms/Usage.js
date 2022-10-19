88e5fa3c18b718bd84c5a9fe1f945a5e8947176f
szlegp <szlegp@gmail.com>
2022-10-19 08:40:27 +0200

    Laptop usage form.

---
+++ src/pages/forms/Usage.js
@@ -0,0 +1,27 @@
+import { Choice, Option } from './Choice';
+
+function Usage() {
+  return <Choice
+  id="internet"
+  options={[
+    new Option(
+      "Aplikacje biurowe i internet",
+      "Word, PowerPoint, Zoom",
+        "usage/office.png"),
+    new Option(
+      "Gry indie i retro",
+      "Minecraft, Roblox, Don’t Starve, Terraria",
+      "usage/indie.png"),
+      new Option(
+        "Modelowanie 3D i digital art",
+        "Blender, Maya, Krita, Photoshop",
+        "usage/modeling.png"),
+      new Option(
+        "Najnowsze gry wysokobudżetowe",
+        "Farcry, Battlefield, Assassin's Creed",
+        "usage/AAA.png")
+  ]}
+  />
+};
+
+export default Usage;
--- src/pages/forms/forms.js
+++ src/pages/forms/forms.js
@@ -5,6 +5,7 @@ import Price from './Price';
 import Data from './Data';
 import Battery from './Battery';
 import Ram from './Ram';
+import Usage from './Usage';
 
 class Form {
     constructor(id, description, element) {
@@ -15,10 +16,11 @@ class Form {
   }
 
 export const forms = [
+    new Form("usage", "Do czego będzie służył laptop", <Usage />),
     new Form("price", "Maksymalna cena", <Price />),
-    new Form("disk", "Rozmiar dysku", <Disk />),
     new Form("screen-extras", "Dodatki do ekranu", <ScreenExtras />),
     new Form("battery", "Czas pracy baterii", <Battery />),
+    new Form("disk", "Rozmiar dysku", <Disk />),
     new Form("internet", "Internet", <Internet />),
     new Form("data", "Przenoszenie danych", <Data />),
     new Form("ram", "Rozmiar pamięci RAM", <Ram />)

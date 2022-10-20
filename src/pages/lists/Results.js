41c5fb4e2aa0acb8441fbdf98ea8901e229d3e78
szlegp <szlegp@gmail.com>
2022-10-20 19:57:44 +0200

    Mock sending form data in console.

--- src/pages/lists/Results.js
+++ src/pages/lists/Results.js
@@ -1,8 +1,16 @@
 import Selection, { makeLaptop } from "./Selection";
 import _ from 'lodash';
 import { getLaptopIds, getLaptop } from '../../api';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function Results() {
+    const forms = useSelector(state=>state.forms);
+
+    useEffect(() => {
+        console.log("Sending form data", forms)
+      }, [forms])
+
     return (
         <div className="content">
             <Selection

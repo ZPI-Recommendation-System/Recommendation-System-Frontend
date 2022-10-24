b20ee567269fd1efa3cd9b2549576b99f3ba07b8
szlegp <szlegp@gmail.com>
2022-10-24 11:31:08 +0200

    Form data processing.

--- src/pages/lists/Results.js
+++ src/pages/lists/Results.js
@@ -4,11 +4,20 @@ import { getLaptopIds, getLaptop } from '../../api';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
+function formDataToRequest(data) {
+    const result = {...data}
+    result["usage"] = Object.entries(result["usage"]).find(e=>e[1])[0]
+    // remove object modifiers 
+    result["size"] = {...result["size"]}
+    delete result["size"]["Kartka A4"]
+    return result
+}
+
 function Results() {
     const forms = useSelector(state=>state.forms);
 
     useEffect(() => {
-        console.log("Sending form data", JSON.stringify(forms))
+        console.log("Sending form data", formDataToRequest(forms))
       }, [forms])
 
     return (

6f16d2dcc03b4040c21222702c26632638becb57
szlegp <szlegp@gmail.com>
2022-10-24 14:18:32 +0200

    Improved numbers display in disk.

--- src/pages/forms/Disk.js
+++ src/pages/forms/Disk.js
@@ -8,12 +8,12 @@ const BATTLEFIELD_4_GB_SIZE = 30
 const BLENDER_3D_GB_SIZE = 0.3
 // https://www.quora.com/What-is-the-average-file-size-of-a-smartphone-camera-picture-in-2018
 const SMARTPHONE_PHOTO_GB_SIZE = 0.0032
-const GAMES_PART = 0.5
-const PROGRAMS_PART = 0.3
-const IMAGES_PART = 0.2
+const GAMES_PART = 0.7
+const PROGRAMS_PART = 0.2
+const IMAGES_PART = 0.1
 
-function gamesByCount(count) {
-    if (count == 1) {
+function gamesPlural(count) {
+    if (count === 1) {
         return "gra";
     } else if (
     // 22 gry
@@ -24,6 +24,13 @@ function gamesByCount(count) {
     } else {
         return "gier";
     }
+}
+
+function roundTo10s(number, shown) {
+    const powerOf10 = Math.ceil(Math.log10(number))
+    const divider = Math.pow(10, Math.max(powerOf10 - shown, 0));
+
+    return Math.floor(number / divider) * divider
 }
 
 function Disk() {
@@ -38,16 +45,16 @@ function Disk() {
     prompt = {"Wybierz minimalny rozmiar wbudowanego dysku"}
     summary = {function (value) {
         const withoutWindows = value - WINDOWS_11_GB_SIZE;
-        const games = Math.floor((GAMES_PART * withoutWindows) / BATTLEFIELD_4_GB_SIZE);
-        const programs = Math.floor((PROGRAMS_PART * withoutWindows) / BLENDER_3D_GB_SIZE);
-        const photos = Math.floor((IMAGES_PART * withoutWindows) / SMARTPHONE_PHOTO_GB_SIZE);
+        const games = roundTo10s((GAMES_PART * withoutWindows) / BATTLEFIELD_4_GB_SIZE, 2);
+        const programs = roundTo10s((PROGRAMS_PART * withoutWindows) / BLENDER_3D_GB_SIZE, 1);
+        const photos = roundTo10s((IMAGES_PART * withoutWindows) / SMARTPHONE_PHOTO_GB_SIZE, 2);
         return <><p className="text">
             Na twoim dysku zmieści się co najmniej: 
         </p>
         <ul className="text">
         <li>Windows 11</li>
         {games > 0 &&
-            <li>{games} {gamesByCount(games)} AAA</li>}
+            <li>{games} {gamesPlural(games)} AAA</li>}
         <li>{programs} programów / gier indie</li>
         <li>{photos} zdjęć / obrazów</li>
         </ul>

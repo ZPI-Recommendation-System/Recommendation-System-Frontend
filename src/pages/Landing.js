1a582b11c8180e6140067d3b7e92ea52187c6d44
szlegp <szlegp@gmail.com>
2022-10-24 11:12:20 +0200

    Links in landing page.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,19 +1,23 @@
+import { Link } from "react-router-dom";
+import { forms } from './forms/forms';
+
 function Landing() {
+    const firstForm = "/"+forms[0].id;
     return (<div className="content">
       <p className="text">
-      Witaj w systemie rekomendacyjnym dla laptopów.
-Wypełnij krótką ankietę aby uzyskać listę wybranych dla ciebie urządzeń.
+        Witaj w systemie rekomendacyjnym dla laptopów.
+        Wypełnij krótką ankietę aby uzyskać listę wybranych dla ciebie urządzeń.
       </p>
-      <p className="skip-button" style={{textAlign: "left"}}    >
+      <Link to={firstForm} className="skip-button" style={{textAlign: "left"}}    >
         Wypełnij ankietę
-      </p>
+      </Link>
       <hr />
       <p className="text" style={{width: "70%"}}>
-      Możesz też wyszukać laptop po nazwie aby móc porównywać go do wybranych przez nas.
+        Możesz też wyszukać laptop po nazwie aby móc porównywać go do wybranych przez nas.
       </p>
-      <p className="skip-button" style={{textAlign: "left"}}>
+      <Link to="/search" className="skip-button" style={{textAlign: "left"}}>
         Wyszukaj po nazwie
-      </p>
+      </Link>
     </div>);
 }
 

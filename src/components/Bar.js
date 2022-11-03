e48a2d60c11d62adcf4a4d3a1016b7357e48d623
szlegp <szlegp@gmail.com>
2022-11-04 00:30:39 +0100

    Moved page logic from Bar to Page.

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,36 +1,7 @@
 import './Bar.css';
-import { Link } from "react-router-dom";  
-import { currentPage, previousPage } from "../pages/pages";   
-import { useSelector, useDispatch } from 'react-redux';
-import { setPage, lastPage, lastFormPage } from '../store/slices/history'; 
-import React, { useEffect, useState, useCallback } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function Bar({index, number, description}) {
-
-    const dispatch = useDispatch();
-
-    const [back, setBack] = useState(null)
-    
-    const lastPage_ = useSelector(lastPage);
-    const lastFormPage_ = useSelector(lastFormPage);
-
-    const previousPageBound = useCallback(
-      ()=> previousPage(index, lastPage_, lastFormPage_),
-      [index, lastPage_, lastFormPage_]);
-    
-    let previousPage_ = back ?? previousPageBound();
-    const currentPage_ = currentPage(index);
-    const isForm = currentPage_.isForm;
-
-    let location = useLocation();
-
-    useEffect(() => {
-      if (lastPage_!==index) {
-        setBack(previousPageBound())
-        dispatch(setPage([index, isForm]))
-      }
-    }, [location, dispatch, index, isForm, previousPageBound, lastPage_]);
+function Bar({number, description, previousPage}) {
 
     const dots = number > 2;
     const mapSymbol = dots ? "..." : "⁖";
@@ -45,8 +16,8 @@ function Bar({index, number, description}) {
                 <span className="progress-bar-divider-space"></span>
             
             {previousLink && <><Link className="progress-bar-link" 
-                to={previousPage_?.link}>
-                {previousPage_?.number}</Link>
+                to={previousPage?.link}>
+                {previousPage?.number}</Link>
                 <span className="progress-bar-divider"></span>
                 <span className="progress-bar-divider-space"></span>
             </>}
--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,8 +1,41 @@
+import { currentPage, previousPage } from "../pages/pages";   
+import { useSelector, useDispatch } from 'react-redux';
+import { setPage, lastPage, lastFormPage } from '../store/slices/history'; 
+import React, { useEffect, useState, useCallback } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import Bar from '../components/Bar';
 
-function Page(props) {
+function Page({index, number, description, children}) {
+    const dispatch = useDispatch();
+
+    const [back, setBack] = useState(null)
+    
+    const lastPage_ = useSelector(lastPage);
+    const lastFormPage_ = useSelector(lastFormPage);
+
+    const previousPageBound = useCallback(
+      ()=> previousPage(index, lastPage_, lastFormPage_),
+      [index, lastPage_, lastFormPage_]);
+    
+    let previousPage_ = back ?? previousPageBound();
+    const currentPage_ = currentPage(index);
+    const isForm = currentPage_.isForm;
+
+    let location = useLocation();
+
+    useEffect(() => {
+      if (lastPage_!==index) {
+        setBack(previousPageBound())
+        dispatch(setPage([index, isForm]))
+      }
+    }, [location, dispatch, index, isForm, previousPageBound, lastPage_]);
+
     return <>
-        <Bar {...props} />{props.children}
+        <Bar 
+            description={description} number={number} 
+            previousPage={previousPage_}
+        />{children}
     </>
 }
 

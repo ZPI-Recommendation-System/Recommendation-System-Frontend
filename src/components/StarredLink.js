f704d6f44fc1fa5671393c15e0a9eeeff9a67901
szlegp <szlegp@gmail.com>
2022-11-04 00:37:08 +0100

    StarredLink useRef and cleanup.

--- src/components/StarredLink.js
+++ src/components/StarredLink.js
@@ -2,26 +2,23 @@ import './StarredLink.css';
 import { Link } from "react-router-dom";
 import { favouritesCount } from '../store/slices/favourites';
 import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function StarredLink() {
     const count = useSelector(favouritesCount);
+    const previousCount = useRef(null);
     const [animate, setAnimate] = useState(false);
-    const [previousCount, setPreviousCount] = useState(null);
 
     useEffect(() => {
-        if (previousCount!==null && count>previousCount) {
+        if (previousCount.current!==null && count>previousCount.current) {
             setAnimate(true)
             // timeout is short and must be invoked 
             // so it's not removed in cleanup
+            // time is copied from animation time
             setTimeout(() =>setAnimate(false), 0.35 * 1000)
         }
-        setPreviousCount(count);
-    }, [count, previousCount])
-
-    useEffect(() => {
-        
-    })
+        previousCount.current = count
+    }, [count])
     
     const favsClass = animate ? "favs favs-pop" : "favs";
 

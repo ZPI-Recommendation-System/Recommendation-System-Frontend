de0d7763400c2df3db3a9c156a955166fa11286f
szlegp <szlegp@gmail.com>
2022-11-04 12:16:45 +0100

    App resize animation.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,44 @@ import Page from './pages/Page';
 
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const container = useRef(null);
+  const [height, setHeight] = useState(0);
+  const style = { height: height + "px" }
+
+  useEffect(() => {
+    if (container.current) {
+      const containerCurrent = container.current;
+
+      const resizeObserver = new ResizeObserver((entries) => {
+        setHeight(containerCurrent.scrollHeight)
+      })
+      resizeObserver.observe(containerCurrent)
+
+      return _ => resizeObserver.unobserve(containerCurrent)
+    }
+  })
+
   return (
-    <div className="app">
-      <Link className="app-title" to='/'>
-        Twój nowy
-        <img className="app-title-icon" src="logo.png" alt="laptop"></img>
-      </Link>
+    <div className="app" style={style}>
+      <div className="app-container" ref={container}>
+        <Link className="app-title" to='/'>
+          Twój nowy
+          <img className="app-title-icon" src="logo.png" alt="laptop"></img>
+        </Link>
         <StarredLink />
-      
 
-      <Routes>
-          {pages.map((page, index)=>
+
+        <Routes>
+          {pages.map((page, index) =>
             <Route key={index} path={page.link} element={
-            <Page {...page} index={index} >{page.element}</Page>} />)
+              <Page {...page} index={index} >{page.element}</Page>} />)
           }
-      </Routes>
+        </Routes>
 
+      </div>
     </div>
   );
 }

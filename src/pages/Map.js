418d9312a1a0bfed7314e22ed5cbe16417dd3760
szlegp <szlegp@gmail.com>
2022-11-14 19:50:46 +0100

    Map circles opacity fix.

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,9 +1,9 @@
 import './Map.css';
 import { Link } from "react-router-dom";
 import { pages } from './pages';
-   
+
 import { useSelector } from 'react-redux';
-import { lastFormPage } from '../store/slices/history'; 
+import { lastFormPage } from '../store/slices/history';
 
 function Map() {
     const lastFormPage_ = useSelector(lastFormPage);
@@ -11,12 +11,14 @@ function Map() {
     return (<div className="content">
         <div className="map-line"></div>
         <div className="map-links-container">
-      {pages.filter(page=>page.isForm || page.link==="/results").map(({number, link, description}, index)=>
-            <Link className="navigation-button" to={link} key={index} >
-                <div className={index>lastFormPage_ ? "map-circle" : "map-circle map-circle-filled"}></div>
-                {number}. {description}
-            </Link>)
-        }
+            {pages.filter(page => page.isForm || page.link === "/results").map(({ number, link, description }, index) =>
+                <div>
+                    <div className={index > lastFormPage_ ? "map-circle" : "map-circle map-circle-filled"}></div>
+                    <Link className="navigation-button" style={{ display: "inline-block" }} to={link} key={index} >
+                        {number}. {description}
+                    </Link>
+                </div>)
+            }
         </div>
     </div>);
 }

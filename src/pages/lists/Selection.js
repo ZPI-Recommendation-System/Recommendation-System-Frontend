1615267b97a86478a5ea7b1583d4df8cc15f2851
szlegp <szlegp@gmail.com>
2022-11-29 15:38:50 +0100

    Sorting in selection (only elements for now).

--- src/pages/lists/Selection.js
+++ src/pages/lists/Selection.js
@@ -15,15 +15,15 @@ function LaptopIcon({ id, name, image, checked, onClick }) {
     </div>;
 }
 
-function Selection({main, extra}) {
+function Selection({ main, extra }) {
     const dispatch = useDispatch();
-    const selected = useSelector(state=>state.selection.selected)
+    const selected = useSelector(state => state.selection.selected)
     const selectedCount = selected.length;
     let prompt = {
-        0 : "Wybierz dwa laptopy aby zobaczyć ich detale i porównanie lub naciśnij gwiazdkę aby zapisać laptop.",
-        1 : "Naciśnij przycisk szczegóły aby zobaczyć detale wybranego laptopa lub dobierz jeszcze jeden dla porównania.",
-        2 : "Naciśnij przycisk porównanie aby zobaczyć porównanie laptopów."
-        }[selectedCount];
+        0: "Wybierz dwa laptopy aby zobaczyć ich detale i porównanie lub naciśnij gwiazdkę aby zapisać laptop.",
+        1: "Naciśnij przycisk szczegóły aby zobaczyć detale wybranego laptopa lub dobierz jeszcze jeden dla porównania.",
+        2: "Naciśnij przycisk porównanie aby zobaczyć porównanie laptopów."
+    }[selectedCount];
 
     function makeIcon(laptop) {
         return <LaptopIcon
@@ -32,34 +32,44 @@ function Selection({main, extra}) {
             name={laptop.name}
             image={laptop.image}
             checked={selected.includes(laptop.id)}
-            onClick = {()=> dispatch(select(laptop.id)) }
+            onClick={() => dispatch(select(laptop.id))}
         />
     }
 
     return (<>
-            <p className="text selection-prompt">
-                {prompt}
-            </p>
-            <div className="selection-container">
+        <p className="text selection-prompt">
+            {prompt}
+        </p>
+        <div className="selection-container">
+        <div></div>
+        <select name="sorting" id="sorting">
+                <option value="fitness">Dopasowanie</option>
+                <option value="popularity">Popularność</option>
+                <option value="price">Cena</option>
+                <option value="name">Nazwa</option>
+            </select>
+            {main.length > 0 &&
+            <>
             <div className="selection-section">
                 {main.map(makeIcon)}
-            </div>
+            </div></>
+        }
             {extra &&
                 <><div className="extras-divider"></div>
-                <p className="extras-text">Te laptopy nie spełniają wszystkich twoich wymagań: </p>
-                <div className="selection-section">
-                    {extra.map(makeIcon)}
-                </div></>}
-            </div>
-            {selectedCount===1
-                && <Link to={"/details/"+selected[0]} className="navigation-button navigation-button-right">
+                    <p className="extras-text">Te laptopy nie spełniają wszystkich twoich wymagań: </p>
+                    <div className="selection-section">
+                        {extra.map(makeIcon)}
+                    </div></>}
+        </div>
+        {selectedCount === 1
+            && <Link to={"/details/" + selected[0]} className="navigation-button navigation-button-right">
                 Szczegóły
-              </Link>}
-              {selectedCount===2
-                && <Link to={"/comparison/"+selected[0]+"/"+selected[1]} className="navigation-button navigation-button-right">
+            </Link>}
+        {selectedCount === 2
+            && <Link to={"/comparison/" + selected[0] + "/" + selected[1]} className="navigation-button navigation-button-right">
                 Porównanie
-              </Link>}
-              </>);
+            </Link>}
+    </>);
 }
 
 export function makeLaptop(index) {

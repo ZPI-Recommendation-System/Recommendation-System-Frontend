11ae3d2a34b833c541d04f22e7451a5b815456f0
szlegp <szlegp@gmail.com>
2022-12-08 18:45:05 +0100

    Finished the gallery.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,16 +1,38 @@
 import './Gallery.css'
 import { useSelector, useDispatch } from 'react-redux';
-import { visible, selectedImage, hide, next } from '../store/slices/gallery';
+import { visible, selectedImage, hide, next, previous } from '../store/slices/gallery';
 
 export default function Gallery() {
   const dispatch = useDispatch();
   const currentlyVisible = useSelector(visible);
   const image = useSelector(selectedImage);
-  
-    const className = currentlyVisible ? "gallery-container" : "gallery-container hidden";
-    return <div className={className} onClick={()=>dispatch(hide())}>
+
+  function handler(action) {
+    return e => {
+      dispatch(action());
+      e.stopPropagation();
+    }
+  }
+
+  const className = currentlyVisible ? "gallery-background" : "gallery-background hidden";
+  return <div className={className} onClick={handler(hide)}>
+    <div className='gallery-container'>
+      <p className='gallery-control gallery-control-close'
+        style={{
+          
+        }}
+      >Zamknij</p>
       <img className="gallery-image"
-      onClick={(event)=>{dispatch(next()); event.stopPropagation();}}
-      alt="laptop-gallery" src={image || ""}></img>
-      </div>
+        onClick={handler(next)}
+        alt="laptop-gallery" src={image || ""}>
+      </img>
+
+      <span className='gallery-control gallery-control-previous'
+        onClick={handler(previous)}
+      >Poprzednie</span>
+      <span className='gallery-control gallery-control-next'
+        onClick={handler(next)}
+      >Następne</span>
+    </div>
+  </div>
 }
